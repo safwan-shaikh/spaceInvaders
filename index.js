635fdf6b1e8b1d8f129807b501a4e9d2bde0657f
safwan-shaikh <mca.2201@unigoa.ac.in>
2023-04-02 15:12:40 +0530

    Complexity changed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 525;
-canvas.height = 500;
+canvas.height = 550;
 
 const background = new Image();
 background.src = "images/space.png";
